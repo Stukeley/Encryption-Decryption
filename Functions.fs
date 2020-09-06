3137102d9ebda4c64eca7dbabc7aba3bc17392a9
Rafał Klinowski <klinowskirafal@gmail.com>
2020-09-06 17:03:53 +0200

    Add project files.

---
+++ Functions.fs
@@ -0,0 +1,85 @@
+﻿module Functions
+
+open System.IO
+
+// For the first stage, you need to manually encrypt the message "we found a treasure!" and print only the ciphertext (in lower case).
+// To encrypt the message, replace each letter with the letter that is in the corresponding position from the end of the English alphabet (a→z, b→y, c→x, ... x→c, y →b, z→a). Do not replace spaces or the exclamation mark.
+// a->z (+25)
+// z->a (-25)
+// b->y (+23)
+// We can add simply the difference between the two letters (it might be negative)
+// if (currentChar >= 97 && currentChar <=122) then
+// replacedChar = 219 - currentChar
+
+let charToAscii (character : char) = int character
+
+let minMaxAscii = (int)('a' + 'z')  // 219
+
+let ReverseCharactersEncryption = 
+
+    let message = "we found a treasure!"
+    let mutable currentChar:char = 'a'
+    let mutable replacedChar:char = 'z'
+    let mutable encrypted:string = ""
+
+    for i in 0 .. message.Length - 1 do
+        currentChar <- message.[i]
+
+        if currentChar >= 'a' && currentChar <= 'z' then
+            replacedChar <- char(minMaxAscii - charToAscii currentChar)
+            encrypted <- encrypted + replacedChar.ToString()
+        else
+            encrypted <- encrypted + currentChar.ToString()
+
+    encrypted
+
+let CaesarEncryption (input:string, key:int32) = 
+
+    let mutable currentChar:char = 'a'
+    let mutable replacedChar:char = 'z'
+    let mutable encrypted:string = ""
+
+    for i in 0 .. input.Length - 1 do
+        currentChar <- input.[i]
+
+        if currentChar >= 'a' && currentChar <= 'z' then
+            let mutable charValue:int = charToAscii currentChar + key
+
+            while charValue > 122 do
+                charValue <- charValue - 26
+
+            replacedChar <- char(charValue)
+            encrypted <- encrypted + replacedChar.ToString()
+        else
+            encrypted <- encrypted + currentChar.ToString()
+
+    encrypted
+
+let CaesarDecryption (input:string, key:int32) = 
+    let mutable currentChar:char = 'a'
+    let mutable replacedChar:char = 'z'
+    let mutable encrypted:string = ""
+
+    for i in 0 .. input.Length - 1 do
+        currentChar <- input.[i]
+
+        if currentChar >= 'a' && currentChar <= 'z' then
+            let mutable charValue:int = charToAscii currentChar - key
+
+            while charValue < 97 do
+                charValue <- charValue + 26
+
+            replacedChar <- char(charValue)
+            encrypted <- encrypted + replacedChar.ToString()
+        else
+            encrypted <- encrypted + currentChar.ToString()
+
+    encrypted
+
+let FileEncryption (inputFile:string, outputFile:string, key:int32) = 
+    use reader = new StreamReader(inputFile)
+    let input = reader.ReadToEnd()
+    let encrypted = CaesarEncryption(input, key)
+
+    use writer = new StreamWriter(outputFile)
+    writer.Write(encrypted)
---
+++ Program.fs
@@ -0,0 +1,63 @@
+﻿// Project: Encryption-Decryption
+// https://hyperskill.org/projects/46
+// Originally to be made in Java, but implemented with F#
+// Privacy is an important matter is the realm of the Internet. When sending a message, you want to be sure that no-one but the addressee with the key can read it. The entirety of the modern Web is encrypted - take https for example! Don’t stay behind: hop on the encryption/decryption train and learn the essential basics while implementing this simple project.
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    // Stage 1
+    printfn "1."
+    printfn "%s" "we found a treasure!"
+    let encrypted = Functions.ReverseCharactersEncryption
+    printfn "%s" encrypted
+    printfn ""
+
+    // Stage 2 & 3
+    printfn "2. & 3."
+    printfn "Type in a sentence, then the key to encrypt it (a positive integer); then enc if you want to encrypt, or dec if you want to decrypt"
+
+    printf "Sentence: "
+    let inputSentence = Console.ReadLine()
+
+    printf "Key: "
+    let mutable key = Convert.ToInt32(Console.ReadLine())
+
+    if key < 0 then
+        printfn "Not a valid key!"
+        key <- 1
+
+    printf "enc/dec: "
+    let algorithmType = Console.ReadLine()
+
+    if algorithmType.ToLower() = "enc" then
+        let result = Functions.CaesarEncryption (inputSentence, key)
+        printfn "%s" result
+    else if algorithmType.ToLower() = "dec" then
+        let result = Functions.CaesarDecryption (inputSentence, key)
+        printfn "%s" result
+        
+
+    printfn ""
+
+    // Stage 5
+    printfn "5."
+    printfn "Encrypting files. Please type in the input file's name, then the output file's name"
+
+    printf "Input: "
+    let inputFileName = Console.ReadLine()
+
+    printf "Output: "
+    let outputFileName = Console.ReadLine()
+
+    printf "Key: "
+    key <- Convert.ToInt32(Console.ReadLine())
+
+    if key < 0 then
+        printfn "Not a valid key!"
+        key <- 1
+
+    Functions.FileEncryption (inputFileName, outputFileName, key)
+
+    0
